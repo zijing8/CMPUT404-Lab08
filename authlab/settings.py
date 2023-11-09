c5d3e92e6958f1304e8c5ba73ec71edb62377944
zijing8 <zijinglu9@gmail.com>
2023-11-08 17:53:47 -0700

    done

--- authlab/settings.py
+++ authlab/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'client.apps.ClientConfig',
+    'rest_framework.authtoken',
+
 ]
 
 MIDDLEWARE = [
@@ -128,5 +130,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+
     )
 }
